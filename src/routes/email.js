701b7b1333874f0ac25f67a52d2bea5a00dc4c35
Kelson Victor <98909425+K3lOli@users.noreply.github.com>
2023-11-13 16:39:04 -0300

    first commit

---
+++ src/routes/email.js
@@ -0,0 +1,9 @@
+const EmailController = require("../controllers/EmailController");
+
+async function EmailRoutes(server) {
+    server.post("/send", async (request, reply) => {
+        EmailController.sendEmail(request, reply);
+    })
+}
+
+module.exports = EmailRoutes;
